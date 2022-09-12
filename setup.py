cff237bedb11a578bf9ca193335c1ea58180aaa4
Husam <39819150+Husam94@users.noreply.github.com>
2022-09-12 16:34:58 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,10 +23,10 @@ setup_requirements = [ ]
 test_requirements = [ ]
 
 setup(
-    author="Husam Abdulnabi",
-    author_email='husam.abdulnabi@gmail.com',
-    python_requires='>=3.7',
-    classifiers=[
+    author = "Husam Abdulnabi",
+    author_email = 'husam.abdulnabi@gmail.com',
+    python_requires = '>=3.7',
+    classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Education', 
@@ -36,7 +36,10 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.9'
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering' 
     ],
     description="Chisel package part of the Poseidon suite",
     install_requires=install_requires,
