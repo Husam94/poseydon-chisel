403559dc30286abf421c4aa56b5c1e1a9081182c
Husam <39819150+Husam94@users.noreply.github.com>
2022-09-12 22:09:36 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=['numpy>=1.20.0' , 
                       'pandas>=1.3.0', 
                       'pysam>=0.18.0', 
-                      'pyBigWig>=0.3.17', 
+                      'pybigwig>=0.3.17', 
                       'biopython>=1.79', 
                       'joblib>=1.1.0', 
                       'matplotlib>=3.5.0', 
