f62599ad5ebe3f01f0a7e252163adb9d46d08837
Husam <39819150+Husam94@users.noreply.github.com>
2022-09-12 21:20:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,6 @@ setup(
     name='poseidon-chisel',
     packages=['poseidon-chisel'],
     url='https://github.com/Husam94/poseidon-chisel',
-    version='0.0.1',
+    version='0.0.3',
     zip_safe=False
 )
