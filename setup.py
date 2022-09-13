a48a133a68626907ea6d21407b996f3b458f9d09
Husam <39819150+Husam94@users.noreply.github.com>
2022-09-12 21:11:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-#!/usr/bin/env python
-
-import io
-from os import path as op
-from setuptools import setup, find_packages
-
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
-here = op.abspath(op.dirname(__file__))
-
-# get the dependencies and installs
-with io.open(op.join(here, "requirements.txt"), encoding="utf-8") as f:
-    all_reqs = f.read().split("\n")
-
-install_requires = [x.strip() for x in all_reqs if "git+" not in x]
-dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
-
-requirements = [ ]
-
-setup_requirements = [ ]
-
-test_requirements = [ ]
+from setuptools import setup
 
 setup(
     author = "Husam Abdulnabi",
@@ -42,19 +20,15 @@ setup(
         'Topic :: Scientific/Engineering' 
     ],
     description="Chisel package part of the Poseidon suite",
-    install_requires=install_requires,
-    dependency_links=dependency_links,
+    install_requires=['numpy' , 'pandas', 'random', 'math', 'pickle', 'pysam', 
+                      'pyBigWig', 'itertools', 'biopython', 'joblib', 'matplotlib', 
+                      'denseweight']
     license="MIT license",
-    long_description=readme,
-    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='poseidon_chisel',
     name='poseidon-chisel',
-    packages=find_packages(include=['poseidon_chisel', 'poseidon_chisel.*']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
+    packages=['poseidon_chisel']
     url='https://github.com/Husam94/poseidon-chisel',
     version='0.0.2',
-    zip_safe=False,
+    zip_safe=False
 )
